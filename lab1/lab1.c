fe6cd6430b1af80b9eab75a3be5ae2f1f36342d2
AntonMakatrov <makatrov_anton@mail.ru>
2021-03-09 09:58:26 +0300

    lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -4,30 +4,8 @@
 #define INT_MAX 32767
 #include "lab1.h"
 
+
 #ifndef TEST
-void FibonacciCounter (int* PointCh1, int* PointCounter, int* PointSum, int* PointEdge)
-{
-  int ch1, ch2, sum, edge, counter;
-
-  counter = 1;
-  sum = 0;
-  ch1 = 0;
-  ch2 = 1;
-  edge = 1;
-  *PointCh1 = ch1;
-  *PointCounter = counter;
-  *PointSum = sum;
-  *PointEdge = edge;
-  do
-  {
-      *PointCh1 = ch2;
-      ch2 = *PointEdge;
-      *PointEdge = *PointCh1 + ch2;
-      *PointSum += *PointCh1;
-      *PointCounter = *PointCounter + 1;
-  } while ( *PointEdge <= INT_MAX );
-}
-
 int main()
 {
     int ch1, sum, edge, counter;
@@ -50,3 +28,26 @@ int main()
 
 }
 #endif
+
+void FibonacciCounter (int* PointCh1, int* PointCounter, int* PointSum, int* PointEdge)
+{
+  int ch1, ch2, sum, edge, counter;
+
+  counter = 1;
+  sum = 0;
+  ch1 = 0;
+  ch2 = 1;
+  edge = 1;
+  *PointCh1 = ch1;
+  *PointCounter = counter;
+  *PointSum = sum;
+  *PointEdge = edge;
+  do
+  {
+      *PointCh1 = ch2;
+      ch2 = *PointEdge;
+      *PointEdge = *PointCh1 + ch2;
+      *PointSum += *PointCh1;
+      *PointCounter = *PointCounter + 1;
+  } while ( *PointEdge <= INT_MAX );
+}
