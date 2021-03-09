197bf7d9a5d5aebf9150e2ba2ce0989c5c8f91f5
AntonMakatrov <makatrov_anton@mail.ru>
2021-03-09 21:02:44 +0300

    lab1

--- lab1/lab1_test.c
+++ lab1/lab1_test.c
@@ -1,16 +1,11 @@
 #include "lab1.h"
-
 #include <stdio.h>
-#include <conio.h>
 #include <assert.h>
-
 #define INT_MAX 32767
-
 
 void testFibonacciCounter (int* PointCh1, int* PointCounter, int* PointSum, int* PointEdge)
 {
   int ch1, sum, edge, counter;
-
   counter = 1;
   sum = 0;
   ch1 = 0;
@@ -21,7 +16,6 @@ void testFibonacciCounter (int* PointCh1, int* PointCounter, int* PointSum, int*
   {
     assert((edge - ch1) == 28657);
     assert((sum - ch1) == 28656);
-
   }
   else
   {
@@ -36,6 +30,5 @@ int main()
   testFibonacciCounter(&ch1, &counter, &sum, &edge);
 
   printf("Tests passed successfully\n");
-  _getch();
   return 0;
 }
