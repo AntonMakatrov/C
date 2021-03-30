1b0ee5e3a48cbb7b7b7e336588f9a13c7cac1441
AntonMakatrov <74259724+AntonMakatrov@users.noreply.github.com>
2021-03-30 14:21:32 +0300

    Add files via upload

---
+++ lab3/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int n, m, i, j;
+int CorrectInput();
+int IsSimilar(int *arr1, int *arr2);
+int IsGreater(int *arr1, int *arr2); 
+void ReadArray(int **arr);
+void WriteArray(int **arr);
+void Sort(int **arr);
+void DeleteSimilar(int **arr);
+ 
+#endif
